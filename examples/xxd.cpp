b6d0fb1e1c5d66955f4c99487743754809105159
Boris Nagaev <bnagaev@gmail.com>
2011-11-16 00:57:27 +0300

    add first example: xxd web interface

---
+++ examples/xxd.cpp
@@ -0,0 +1,28 @@
+
+#include <Wt/WApplication>
+#include <Wt/Wc/Wbi.hpp>
+
+using namespace Wt;
+using namespace Wt::Wc;
+
+class XxdApp : public WApplication {
+public:
+    XxdApp(const WEnvironment& env):
+        WApplication(env) {
+        enableUpdates(true);
+        TableTask* task = new TableTask(root());
+        task->add_input(new FileInput(""), "Binary file");
+        task->add_output(new ViewFileOutput(""), "Hex dump");
+        task->set_runner(new ForkingTaskRunner("xxd"));
+        task->finished().connect(this, &WApplication::triggerUpdate);
+    }
+};
+
+WApplication* createApplication(const WEnvironment& env) {
+    return new XxdApp(env);
+}
+
+int main(int argc, char** argv) {
+    return WRun(argc, argv, &createApplication);
+}
+
