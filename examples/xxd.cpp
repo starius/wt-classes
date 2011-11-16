519fde91856064aed59b4e8919b830e475a06561
Boris Nagaev <bnagaev@gmail.com>
2011-11-16 23:14:03 +0300

    examples/xxd: use < and > for stdin and stdout
    This turned out to work!
    
    close #3

--- examples/xxd.cpp
+++ examples/xxd.cpp
@@ -13,8 +13,8 @@ public:
         messageResourceBundle().use(Wt::WApplication::appRoot() +
                                     "locales/wtclasses");
         TableTask* task = new TableTask(root());
-        task->add_input(new FileInput(""), "Binary file");
-        task->add_output(new ViewFileOutput(""), "Hex dump");
+        task->add_input(new FileInput("<"), "Binary file");
+        task->add_output(new ViewFileOutput(">"), "Hex dump");
         task->set_runner(new ForkingTaskRunner("xxd"));
         task->finished().connect(this, &WApplication::triggerUpdate);
     }
