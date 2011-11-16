f6aab7665bdc80ce8ab6a43f3dc43cc18f536fb6
Boris Nagaev <bnagaev@gmail.com>
2011-11-17 01:57:27 +0300

    add util.hpp with isinstance() function

---
+++ src/util.hpp
@@ -0,0 +1,34 @@
+/*
+ * wt-classes, utility classes used by Wt applications
+ * Copyright (C) 2011 Boris Nagaev
+ *
+ * wt-classes is licensed under the GNU GPL Version 2.
+ * Other versions of the GPL do not apply.
+ * See the LICENSE file for terms of use.
+ */
+
+#ifndef WT_UTIL_HPP_
+#define WT_UTIL_HPP_
+
+namespace Wt {
+
+namespace Wc {
+
+/** Return whether an object is an instance of a class or of a subclass thereof.
+This function and it's documentation was copied from python.
+*/
+template <class T, class S>
+bool isinstance(const S* object) {
+    try {
+        return dynamic_cast<const T*>(object) != 0;
+    } catch (...) {
+        return false;
+    }
+}
+
+}
+
+}
+
+#endif
+
