93fb436f05c3a38764967739d40dbe6be0b9db08
Boris Nagaev <bnagaev@gmail.com>
2011-12-21 09:53:06 +0300

    remove reference to gpl license from all files except LICENSE

--- src/util.hpp
+++ src/util.hpp
@@ -2,8 +2,6 @@
  * wt-classes, utility classes used by Wt applications
  * Copyright (C) 2011 Boris Nagaev
  *
- * wt-classes is licensed under the GNU GPL Version 2.
- * Other versions of the GPL do not apply.
  * See the LICENSE file for terms of use.
  */
 
