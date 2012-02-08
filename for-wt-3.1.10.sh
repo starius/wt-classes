e7194470ba9d71d8ea738ce1404190a2e8ec10d0
Boris Nagaev <bnagaev@gmail.com>
2012-02-08 18:02:52 +0400

    change for-wt-3.1.10.sh to be cun in the top directory

--- for-wt-3.1.10.sh
+++ for-wt-3.1.10.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm Planning.?pp                       # depends on WIOService
-sed '/WLink /,/}/d' -i Url.cpp        # remove methods returning WLink
-sed '/WLink/d' -i Url.hpp             # remove methods returning WLink
+rm src/Planning.?pp                            # depends on WIOService
+sed '/WLink /,/}/d' -i src/Url.cpp             # remove methods returning WLink
+sed '/WLink/d' -i src/Url.hpp                  # remove methods returning WLink
 
