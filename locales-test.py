ca2760a5f6bb44dab4c3429014dc9633f69d7c0e
Boris Nagaev <bnagaev@gmail.com>
2012-02-08 21:57:43 +0400

    locales-test: use a function for error reporting
    see #18

--- locales-test.py
+++ locales-test.py
@@ -11,12 +11,28 @@ from xml.etree.ElementTree import parse
 from StringIO import StringIO
 import fnmatch
 import os
+import sys
 import re
 import argparse
 
 ampersand = '-locales_test_ampersand'
 
+def error(stream, message, type='error', file=None, line=None, id=None):
+    if id:
+        if file:
+            stream.write(file + ': ')
+        stream.write('In message "%s":\n' % id)
+    if file:
+        stream.write(file + ':')
+        if line:
+            stream.write(str(line + 1) + ':')
+        stream.write(' ')
+    stream.write(type + ': ' + message + "\n")
+
 def locales_test(wt, prefix, sections):
+    def e(*args, **kwargs):
+        error(sys.stderr, *args, **kwargs)
+
     wt_ids = set()
     for wt_xml in (wt or []):
         try:
@@ -24,7 +40,7 @@ def locales_test(wt, prefix, sections):
             for message in wt_messages:
                 wt_ids.add(message.get('id'))
         except:
-            print 'Error: provide correct wt.xml file through --wt option'
+            e('provide correct wt.xml file through --wt option')
             return
 
     filename2ids = {}
@@ -37,11 +53,11 @@ def locales_test(wt, prefix, sections):
         path = os.path.join('locales', filename)
         xml_file = codecs.open(path, 'r', 'utf-8')
         if xml_file.read().count('\t'):
-            print filename, 'Error: no tabs are allowed'
+            e('no tabs are allowed', file=filename)
         xml_file.seek(0)
-        for line in xml_file:
+        for line_index, line in enumerate(xml_file):
             if len(line) > 120:
-                print filename, 'Error: line "%s" is too long' % line
+                e('the line is too long', file=filename, line=line_index)
         contents = open(path).read().replace('&', ampersand).replace('if:', '')
         xml_file = StringIO(contents)
         messages = parse(xml_file).getroot()
@@ -52,63 +68,61 @@ def locales_test(wt, prefix, sections):
                 message.text = ''
             text = message.text
             if message.tag != 'message':
-                print filename, Id, 'Error: wrong tag "%s"' % message.tag
+                e('wrong tag "%s"' % message.tag, file=filename, id=Id)
             if text and '\n' not in text and (text[0] == ' ' or text[-1] == ' '):
-                print filename, Id, \
-                        'Error: message should not start or end with space'
+                e('message should not start or end with space',
+                        file=filename, id=Id)
             if Id in ids:
-                print filename, Id, \
-                        'Error: duplicate message'
+                e('duplicate message', file=filename, id=Id)
             ids.add(Id)
             id2text[Id] = text
             if not Id.startswith('Wt.') and not Id.startswith(prefix):
-                print filename, Id, \
-                    'Error: message id should start with Wt. or %s' % prefix
+                e('message id should start with Wt. or %s' % prefix,
+                        file=filename, id=Id)
             if Id.startswith('Wt.'):
                 if Id not in wt_ids:
                     if wt_ids:
-                        print filename, Id, \
-                                'Error: unknown Wt message (old wt.xml?)'
+                        e('unknown Wt message (old wt.xml?)',
+                                file=filename, id=Id)
                     else:
-                        print filename, Id, \
-                                'Error: unknown Wt message (provide --wt option)'
+                        e('unknown Wt message (provide --wt option)',
+                                file=filename, id=Id)
             else:
                 try:
                     prefix_copy, section, id = Id.split('.')
                 except:
-                    print filename, Id, \
-                            "Error: can't match to %s.section.id" % prefix
+                    e("can't match to %s.section.id" % prefix,
+                            file=filename, id=Id)
                 message.section = section
                 if section not in sections:
-                    print filename, Id, 'Error: unknown section "%s"' % section
+                    e('unknown section "%s"' % section, file=filename, id=Id)
                 if '-' in id:
-                    print filename, Id, 'Error: id should not contain "-"'
+                    e('id should not contain "-"', file=filename, id=Id)
                 if text and id[0].islower() != text[0].islower() and \
                         text[0].isalpha():
-                    print filename, Id, \
-                        'Error: id should start with letter of same case as message'
+                    e('id should start with letter of same case as message',
+                            file=filename, id=Id)
                 if '${' in text and not id.endswith('_template'):
-                    print filename, Id, \
-                        'Error: id of template message should end with "_template"'
+                    e('id of template message should end with "_template"',
+                            file=filename, id=Id)
                 if hasattr(prev_message, 'section'):
                     if prev_message.section != section and \
                             '\n\n' not in prev_message.tail:
-                        print filename, ('Error: sections %s and %s should be ' + \
-                            'separated by empty line') % \
-                            (prev_message.section, section)
+                        e('separate sections %s and %s with empty line' %
+                                (prev_message.section, section),
+                                file=filename, id=Id)
                     elif prev_message.section == section:
                         if '\n\n' in prev_message.tail:
-                            print filename, ('Error: section %s should not be ' + \
-                                'separated with empty line near %s') % \
-                                (section, Id)
+                            e('do not separate section %s' % section,
+                                    file=filename, id=Id)
                         if '\n' in prev_message.text and '\n' not in message.text:
-                            print filename, ('Error: non-multiline message %s ' + \
-                                'follows multimessages') % Id
+                            e('non-multiline message after multiline messages',
+                                    file=filename, id=Id)
                         if '\n' in prev_message.text and '\n' in message.text \
                             and prev_message.get('id') > Id and not 'board' in Id:
-                            print filename, ('Error: multiline messages ' + \
-                                '%s and %s are unordered') % \
-                                (prev_message.get('id'), Id)
+                            e('multiline messages %s and %s are unordered' %
+                                    (prev_message.get('id'), Id),
+                                    file=filename, id=Id)
 
             prev_message = message
 
@@ -117,8 +131,8 @@ def locales_test(wt, prefix, sections):
                 m.get('id').startswith(prefix)]
         for m1, m2 in zip(short_messages, sorted(short_messages)):
             if m1 != m2:
-                print filename, \
-                    'Error: messages are unsorted, started from %s != %s' % (m1, m2)
+                e('messages are unsorted, started from %s != %s' % (m1, m2),
+                        file=filename)
 
     all_ids = reduce(lambda a,b:a|b, filename2ids.values(), set())
     cleaner = re.compile(r'{.+}|<.+>|%s.{1,8};' % ampersand)
@@ -129,7 +143,8 @@ def locales_test(wt, prefix, sections):
                 continue
             if not any(c.isalpha() for c in re.sub(cleaner, "", id2text[Id])):
                 continue
-            print filename, 'Warning: id "%s" not found' % Id
+            e('the id not found in some .xml files', type='warning',
+                    file=filename, id=Id)
 
     l9n_re = re.compile(r'"(%s\.[^"]+)"' % re.escape(prefix))
     used_ids = set()
@@ -140,11 +155,11 @@ def locales_test(wt, prefix, sections):
             for match in re.findall(l9n_re, open(path).read()):
                 used_ids.add(match)
                 if match not in all_ids:
-                    print path, "Error: can't find message for id '%s'" % match
+                    e("can't find message for the id", file=path, id=match)
 
     for Id in all_ids - used_ids:
         if not Id.startswith('Wt.'):
-            print 'Warning: id "%s" defined but not used' % Id
+            e('the id defined but not used', type='warning', id=Id)
 
 def main():
     p = argparse.ArgumentParser(description='Wt l9n checker')
