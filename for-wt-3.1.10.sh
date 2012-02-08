8af6bdc32d58f1c48e3e6ff67e0415abfd09c70f
Boris Nagaev <bnagaev@gmail.com>
2012-02-08 18:10:00 +0400

    update for-wt-3.1.10 with examples/url

--- for-wt-3.1.10.sh
+++ for-wt-3.1.10.sh
@@ -3,4 +3,6 @@
 rm src/Planning.?pp                            # depends on WIOService
 sed '/WLink /,/}/d' -i src/Url.cpp             # remove methods returning WLink
 sed '/WLink/d' -i src/Url.hpp                  # remove methods returning WLink
+sed 's/link/full_path/g' -i examples/url.cpp   # use string instead of WLink
+sed 's/setLink/setRefInternalPath/g' -i examples/url.cpp
 
