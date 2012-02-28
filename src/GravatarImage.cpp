09aca3b02cb466c63c2b50afc33f21863b13d2a8
Boris Nagaev <bnagaev@gmail.com>
2012-02-28 04:00:35 +0400

    add GravatarImage
    close #36

---
+++ src/GravatarImage.cpp
@@ -0,0 +1,140 @@
+/*
+ * wt-classes, utility classes used by Wt applications
+ * Copyright (C) 2011 Boris Nagaev
+ *
+ * See the LICENSE file for terms of use.
+ */
+
+#include <sstream>
+#include <boost/algorithm/string/trim.hpp>
+#include <boost/algorithm/string/case_conv.hpp>
+
+#include <Wt/WApplication>
+#include <Wt/WEnvironment>
+
+#include "GravatarImage.hpp"
+#include "util.hpp"
+
+namespace Wt {
+
+namespace Wc {
+
+const GravatarImage::Rating GRAVATAR_DEFAULT_RATING = GravatarImage::G;
+const short GRAVATAR_DEFAULT_SIZE = 80;
+
+GravatarImage::GravatarImage(const std::string& email,
+                             WContainerWidget* parent):
+    WImage(parent),
+    size_(GRAVATAR_DEFAULT_SIZE),
+    rating_(GRAVATAR_DEFAULT_RATING),
+    force_default_(false),
+    secure_requests_(INHERIT) {
+    set_email(email);
+    resize_image(size_);
+}
+
+void GravatarImage::set_email(const std::string& email) {
+    using namespace boost::algorithm;
+    email_ = email;
+    trim(email_);
+    to_lower(email_);
+    update_url();
+}
+
+void GravatarImage::set_size(const WLength& size) {
+    size_ = size.toPixels();
+    update_url();
+    resize_image(size);
+}
+
+void GravatarImage::set_default(const WLink& link) {
+    default_ = link.url();
+    update_url();
+}
+
+void GravatarImage::set_default(BuiltIn option) {
+    if (option == DEFAULT) {
+        default_ = "";
+    } else if (option == RETURN_404) {
+        default_ = "404";
+    } else if (option == MM) {
+        default_ = "mm";
+    } else if (option == IDENTICON) {
+        default_ = "identicon";
+    } else if (option == MONSTERID) {
+        default_ = "monsterid";
+    } else if (option == WAVATAR) {
+        default_ = "wavatar";
+    } else if (option == RETRO) {
+        default_ = "retro";
+    }
+    update_url();
+}
+
+void GravatarImage::set_rating(Rating rating) {
+    rating_ = rating;
+    update_url();
+}
+
+void GravatarImage::set_force_default(bool force_default) {
+    force_default_ = force_default;
+    update_url();
+}
+
+void GravatarImage::set_secure_requests(SecureRequests secure_requests) {
+    secure_requests_ = secure_requests;
+    update_url();
+}
+
+void GravatarImage::update_url() {
+    std::stringstream url;
+    if (https()) {
+        url << "https://secure.";
+    } else {
+        url << "http://www.";
+    }
+    url << "gravatar.com/avatar/";
+    url << md5(email_);
+    url << ".jpg";
+    url << "?";
+    if (size_ != GRAVATAR_DEFAULT_SIZE) {
+        url << "s=" << TO_S(size_) << "&";
+    }
+    if (!default_.empty()) {
+        url << "d=" << urlencode(default_) << "&";
+    }
+    if (rating_ != GRAVATAR_DEFAULT_RATING) {
+        url << "r=" << rating_str() << "&";
+    }
+    if (force_default_) {
+        url << "f=y";
+    }
+    setImageLink(url.str());
+}
+
+void GravatarImage::resize_image(const WLength& size) {
+    resize(size, size);
+}
+
+bool GravatarImage::https() const {
+    return secure_requests_ == ALWAYS ||
+           (secure_requests_ == INHERIT &&
+            wApp->environment().urlScheme() == "https");
+}
+
+std::string GravatarImage::rating_str() const {
+    std::string result = "g";
+    if (rating_ == PG) {
+        result = "pg";
+    } else if (rating_ == R) {
+        result = "r";
+    } else if (rating_ == X) {
+        result = "x";
+    }
+    return result;
+}
+
+}
+
+}
+
---
+++ src/GravatarImage.hpp
@@ -0,0 +1,128 @@
+/*
+ * wt-classes, utility classes used by Wt applications
+ * Copyright (C) 2011 Boris Nagaev
+ *
+ * See the LICENSE file for terms of use.
+ */
+
+#ifndef WC_GRAVATAR_IMAGE_HPP_
+#define WC_GRAVATAR_IMAGE_HPP_
+
+#include <Wt/WGlobal>
+#include <Wt/WImage>
+
+namespace Wt {
+
+namespace Wc {
+
+/** Show an avatar from Gravatar.
+
+For more information, see http://gravatar.com/site/implement/images/
+
+\ingroup bindings
+*/
+class GravatarImage : public WImage {
+public:
+    /** If an image is appropriate for a certain audience.
+    The image is selt-rated by a user uploaded it to Gravatar.
+
+    The definitions were copied from Gravatar documentation.
+    */
+    enum Rating {
+        G,  /**< Suitable for display on all websites with any audience type.
+                 The default rating */
+        PG, /**< May contain rude gestures, provocatively dressed individuals,
+                 the lesser swear words, or mild violencea */
+        R,  /**< May contain such things as harsh profanity, intense violence,
+                 nudity, or hard drug use */
+        X   /**< May contain hardcore sexual imagery or
+                 extremely disturbing violence */
+    };
+
+    /** Built in options for default avatar.
+
+    Some definitions were copied from Gravatar documentation.
+    */
+    enum BuiltIn {
+        DEFAULT,    /**< Standard default image */
+        RETURN_404, /**< Do not load any image if none is associated
+                         with the email hash, instead return an HTTP 404
+                         (File Not Found) response */
+        MM,         /**< (mystery-man) a simple, cartoon-style silhouetted
+                          outline of a person (does not vary by email hash) */
+        IDENTICON,  /**< A geometric pattern based on an email hash */
+        MONSTERID,  /**< A generated 'monster' with different
+                         colors, faces, etc */
+        WAVATAR,    /**< Generated faces with differing
+                         features and backgrounds */
+        RETRO,      /**< Awesome generated,
+                         8-bit arcade-style pixelated faces */
+    };
+
+    /** Whether to use secure requests (https) to Gravatar */
+    enum SecureRequests {
+        INHERIT, /**< Use the same scheme, as the Wt application does.
+                     The default variant.
+                     \see WEnvironment::urlScheme() */
+        ALWAYS,  /**< Always use https */
+        NEVER    /**< Always use http */
+    };
+
+    /** Constructor.
+    \param email  E-mail address of the user (may be empty or incorrect).
+    \param parent Parent widget.
+    */
+    GravatarImage(const std::string& email, WContainerWidget* parent = 0);
+
+    /** Set e-mail address of the user */
+    void set_email(const std::string& email);
+
+    /** Set image height and width.
+    The size may be anywhere from 1px up to 512px.
+    Defaultss to 80px.
+    */
+    void set_size(const WLength& size);
+
+    /** Set default image.
+    By default, Gravatar returns <a href=http://www.gravatar.com/avatar/>
+    this image</a> if an email address has no matching Gravatar image.
+
+    You can use this method to change it to other default image.
+    */
+    void set_default(const WLink& link);
+
+    /** Set default built-in image.
+
+    \see set_default(const WLink&)
+    */
+    void set_default(BuiltIn option);
+
+    /** Set rating */
+    void set_rating(Rating rating);
+
+    /** Set whether the default image should be returned for any e-mail */
+    void set_force_default(bool force_default);
+
+    /** Set http/https selection rules */
+    void set_secure_requests(SecureRequests secure_requests);
+
+private:
+    std::string email_;
+    short size_;
+    std::string default_;
+    Rating rating_;
+    bool force_default_;
+    SecureRequests secure_requests_;
+
+    void update_url();
+    void resize_image(const WLength& size);
+    bool https() const;
+    std::string rating_str() const;
+};
+
+}
+
+}
+
+#endif
+
