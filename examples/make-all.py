64b6889f58026a9b147c95a72535a4a5b73bc2e4
Boris Nagaev <bnagaev@gmail.com>
2012-04-05 22:13:54 +0400

    change make-all.py to be run without sed

--- examples/make-all.py
+++ examples/make-all.py
@@ -7,8 +7,11 @@ import re
 entrypoints = []
 anchors = []
 
+remove_main = re.compile("int main.+\}", re.DOTALL)
+
 for cpp in glob.glob('examples/*.cpp'):
     if cpp != 'examples/all.cpp':
+        sys.stdout.write(remove_main.sub("", open(cpp).read()))
         low = cpp.split('/')[-1].split('.')[0] # name without path and extension
         Cap = re.search(r"create([^\s]+)App", open(cpp).read()).groups()[0]
         args = {'low': low, 'Cap': Cap}
@@ -19,6 +22,6 @@ for cpp in glob.glob('examples/*.cpp'):
         new WAnchor("%(low)s", "%(Cap)s", root());
         new WBreak(root());
         ''' % args)
-sys.stdout.write(sys.stdin.read() % {'entrypoints': ''.join(entrypoints),
-    'anchors': ''.join(anchors)})
+sys.stdout.write(open('examples/all.cpp.in').read() %
+    {'entrypoints': ''.join(entrypoints), 'anchors': ''.join(anchors)})
 
