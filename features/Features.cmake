807f1edef1ebcdb2ea2318c947d070f90951c042
Boris Nagaev <bnagaev@gmail.com>
2012-04-07 01:40:33 +0400

    cmake: add WRandom feature test

--- features/Features.cmake
+++ features/Features.cmake
@@ -6,6 +6,7 @@ set(CMAKE_REQUIRED_LIBRARIES ${CMAKE_REQUIRED_LIBRARIES} wt)
 check_include_file_cxx(Wt/WLink HAVE_WLINK)
 check_include_file_cxx(Wt/WIOService HAVE_WIOSERVICE)
 check_include_file_cxx(Wt/Http/Message HAVE_WHTTP_MESSAGE)
+check_include_file_cxx(Wt/WRandom HAVE_WRANDOM)
 check_cxx_source_compiles("#include <Wt/WFormWidget>\n int main() {
     Wt::WFormWidget* a; a->valueText(); }" HAVE_WFORMWIDGET_VALUETEXT)
 check_cxx_source_compiles("#include <Wt/WApplication>\n int main() {
