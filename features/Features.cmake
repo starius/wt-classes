2b0dc7db4aa224201010673bf678497269e815f7
Boris Nagaev <bnagaev@gmail.com>
2012-04-07 01:22:17 +0400

    cmake: add appRoot() feature test

--- features/Features.cmake
+++ features/Features.cmake
@@ -8,4 +8,6 @@ check_include_file_cxx(Wt/WIOService HAVE_WIOSERVICE)
 check_include_file_cxx(Wt/Http/Message HAVE_WHTTP_MESSAGE)
 check_cxx_source_compiles("#include <Wt/WFormWidget>\n int main() {
     Wt::WFormWidget* a; a->valueText(); }" HAVE_WFORMWIDGET_VALUETEXT)
+check_cxx_source_compiles("#include <Wt/WApplication>\n int main() {
+    Wt::WApplication::appRoot(); } " HAVE_WAPPLICATION_APPROOT)
 
