e271bf9144babb336427c680141395b360034a78
Boris Nagaev <bnagaev@gmail.com>
2012-04-08 02:38:47 +0400

    update TimeDurationWidget to be compilable without W[Double]SpinBox

--- src/TimeDurationWidget.cpp
+++ src/TimeDurationWidget.cpp
@@ -5,11 +5,17 @@
  * See the LICENSE file for terms of use.
  */
 
+#include <boost/lexical_cast.hpp>
+
 #include <Wt/WContainerWidget>
+#include <Wt/WLineEdit>
+#include <Wt/WDoubleValidator>
 #include <Wt/WComboBox>
 
 #include "TimeDurationWidget.hpp"
 #include "ConstrainedSpinBox.hpp"
+#include "util.hpp"
+#include "config.hpp"
 
 namespace Wt {
 
@@ -26,9 +32,18 @@ public:
                            const TimeDuration& max) :
         WContainerWidget(), min_(min), max_(max), unit_(SECOND) {
         setInline(true);
+#if defined(HAVE_WDOUBLESPINBOX)
         spin_box_ = new ConstrainedDoubleSpinBox(this);
-        spin_box_->setRange(min.total_seconds(), max.total_seconds());
-        spin_box_->setValue(value.total_seconds());
+#elif defined(HAVE_WSPINBOX)
+        spin_box_ = new ConstrainedSpinBox(this);
+#else
+        spin_box_ = new WLineEdit(this);
+        spin_box_->setValidator(new WDoubleValidator(spin_box_));
+        // set parent of WDoubleValidator to prevent memory leak
+        // see http://redmine.emweb.be/boards/2/topics/544
+#endif
+        set_raw_value(min.total_seconds(), value.total_seconds(),
+                      max.total_seconds());
         combo_box_ = new WComboBox(this);
         combo_box_->addItem(tr("wc.time.seconds"));
         combo_box_->addItem(tr("wc.time.minutes"));
@@ -47,8 +62,7 @@ public:
     }
 
     TimeDuration value() const {
-        return spin_box_->value() *
-               INTERVALS[combo_box_->currentIndex()];
+        return raw_value() * INTERVALS[combo_box_->currentIndex()];
     }
 
     TimeDuration corrected_value() const {
@@ -72,17 +86,44 @@ public:
     }
 
 private:
+#if defined(HAVE_WDOUBLESPINBOX)
     ConstrainedDoubleSpinBox* spin_box_;
+#elif defined(HAVE_WSPINBOX)
+    ConstrainedSpinBox* spin_box_;
+#else
+    WLineEdit* spin_box_;
+#endif
     WComboBox* combo_box_;
     TimeDuration min_;
     TimeDuration max_;
     TimeDuration unit_;
 
     void select_handler_(int i) {
-        TimeDuration s = spin_box_->corrected_value() * unit_;
+        TimeDuration s = raw_value() * unit_;
         unit_ = INTERVALS[i];
-        spin_box_->setRange(min_ / unit_, max_ / unit_);
-        spin_box_->setValue(s / unit_);
+        set_raw_value(min_ / unit_, s / unit_, max_ / unit_);
+    }
+
+    void set_raw_value(double min, double value, double max) {
+#if defined(HAVE_WDOUBLESPINBOX) || defined(HAVE_WSPINBOX)
+        spin_box_->setRange(min, max);
+        spin_box_->setValue(value);
+#else
+        spin_box_->setText(TO_S(value));
+        DOWNCAST<WDoubleValidator*>(spin_box_->validator())->setRange(min, max);
+#endif
+    }
+
+    double raw_value() const {
+#if defined(HAVE_WDOUBLESPINBOX) || defined(HAVE_WSPINBOX)
+        return spin_box_->value();
+#else
+        try {
+            return boost::lexical_cast<double>(spin_box_->text().toUTF8());
+        } catch (...) {
+            return 0;
+        }
+#endif
     }
 };
 
