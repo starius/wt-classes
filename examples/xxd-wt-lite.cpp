6fd48195097418f396e6fe10d02706ad2922f42d
Boris Nagaev <bnagaev@gmail.com>
2012-04-08 22:42:29 +0400

    use my approot() in xxd-wt-lite example

--- examples/xxd-wt-lite.cpp
+++ examples/xxd-wt-lite.cpp
@@ -1,6 +1,7 @@
 
 #include <Wt/WApplication>
 #include <Wt/Wc/Wbi.hpp>
+#include <Wt/Wc/util.hpp>
 
 using namespace Wt;
 using namespace Wt::Wc;
@@ -10,8 +11,7 @@ public:
     XxdLiteApp(const WEnvironment& env):
         WApplication(env) {
         enableUpdates(true);
-        messageResourceBundle().use(Wt::WApplication::appRoot() +
-                                    "locales/wtclasses");
+        messageResourceBundle().use(approot() + "locales/wtclasses");
         TableTask* task = new TableTask(root());
         task->add_input(new FileInput("<"), "Binary file");
         task->add_output(new ViewFileOutput(">"), "Hex dump");
