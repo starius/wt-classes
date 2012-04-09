c1ee1b18bc387724cacf5ab8acef06624e55a4ce
Boris Nagaev <bnagaev@gmail.com>
2012-04-10 00:28:12 +0400

    remove downgrading scripts and configure.py, update INSTALL
    close #81

--- no-openssl.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm src/GravatarImage.?pp                            # no md5
-rm examples/gravatar.cpp                            # no GravatarImage
-sed '/std::string md5/,/^$/d' -i src/util.?pp                   # no md5
-sed '/ssl/d' -i Makefile INSTALL debian/control.in src/util.cpp # no openssl
-
