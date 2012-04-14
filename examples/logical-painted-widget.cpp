7eedc9291cd7ca53c7111763cdd93d15bfc19baa
Boris Nagaev <bnagaev@gmail.com>
2012-04-14 23:00:41 +0400

    add example logical-painted-widget
    close #59

---
+++ examples/logical-painted-widget.cpp
@@ -0,0 +1,46 @@
+
+#include <Wt/WApplication>
+#include <Wt/WPaintedWidget>
+#include <Wt/WPainter>
+#include <Wt/WPainterPath>
+#include <Wt/Wc/LogicalPaintedWidget.hpp>
+
+using namespace Wt;
+using namespace Wt::Wc;
+
+class TestPainting : public LogicalPaintedWidget {
+public:
+    TestPainting(WContainerWidget* p = 0) :
+        LogicalPaintedWidget(p) {
+        resize(500, 500);
+        set_logical_window(Wt::WRectF(0, 0, 1.0, 1.0));
+    }
+
+protected:
+    void paintEvent(WPaintDevice* device) {
+        Wt::WPainter painter(device);
+        use_logical(painter);
+        painter.drawEllipse(0, 0, 1, 1);
+        Wt::WPainterPath path;
+        path.moveTo(0, 0);
+        path.lineTo(1, 1);
+        painter.drawPath(path);
+    }
+};
+
+class LogicalPaintedApp : public WApplication {
+public:
+    LogicalPaintedApp(const WEnvironment& env):
+        WApplication(env) {
+        new TestPainting(root());
+    }
+};
+
+WApplication* createLogicalPaintedApp(const WEnvironment& env) {
+    return new LogicalPaintedApp(env);
+}
+
+int main(int argc, char** argv) {
+    return WRun(argc, argv, &createLogicalPaintedApp);
+}
+
