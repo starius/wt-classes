d6319297e5c74f27a729c6b72bb563992d6eec03
Boris Nagaev <bnagaev@gmail.com>
2012-04-28 15:17:18 +0400

    feature test: perform compilation tests instead of headers detection
    Pros:
     * more intensive tests
     * same tests (there were already some compilation tests)

--- features/Features.cmake
+++ features/Features.cmake
@@ -1,17 +1,24 @@
-include(CheckIncludeFileCXX)
 include(CheckCXXSourceCompiles)
 include(FindOpenSSL)
 
 set(CMAKE_REQUIRED_LIBRARIES ${CMAKE_REQUIRED_LIBRARIES}
     wt boost_filesystem boost_system)
 
-check_include_file_cxx(Wt/WLink WC_HAVE_WLINK)
-check_include_file_cxx(Wt/WIOService WC_HAVE_WIOSERVICE -lboost_system)
-check_include_file_cxx(Wt/Http/Message WC_HAVE_WHTTP_MESSAGE)
-check_include_file_cxx(Wt/WRandom WC_HAVE_WRANDOM)
-check_include_file_cxx(Wt/WRasterImage WC_HAVE_WRASTERIMAGE)
-check_include_file_cxx(Wt/WSpinBox WC_HAVE_WSPINBOX)
-check_include_file_cxx(Wt/WDoubleSpinBox WC_HAVE_WDOUBLESPINBOX)
+check_cxx_source_compiles("#include <Wt/WLink>\n int main() {
+    Wt::WLink l; }" WC_HAVE_WLINK)
+check_cxx_source_compiles("#include <Wt/WIOService>\n int main() {
+    Wt::WIOService io; }" WC_HAVE_WIOSERVICE)
+check_cxx_source_compiles("#include <Wt/Http/Message>\n int main() {
+    Wt::Http::Message m; }" WC_HAVE_WHTTP_MESSAGE)
+check_cxx_source_compiles("#include <Wt/WRandom>\n int main() {
+    Wt::WRandom::get(); }" WC_HAVE_WRANDOM)
+check_cxx_source_compiles("#include <Wt/WRasterImage>\n
+    int main() { Wt::WRasterImage r(\"\",0,0); }"
+    WC_HAVE_WRASTERIMAGE)
+check_cxx_source_compiles("#include <Wt/WSpinBox>\n int main() {
+    Wt::WSpinBox box; }" WC_HAVE_WSPINBOX)
+check_cxx_source_compiles("#include <Wt/WDoubleSpinBox>\n int main() {
+    Wt::WDoubleSpinBox box; }" WC_HAVE_WDOUBLESPINBOX)
 check_cxx_source_compiles("#include <Wt/WFormWidget>\n int main() {
     Wt::WFormWidget* a; a->valueText(); }" WC_HAVE_WFORMWIDGET_VALUETEXT)
 check_cxx_source_compiles("#include <Wt/WApplication>\n int main() {
@@ -31,8 +38,8 @@ check_cxx_source_compiles("#include <Wt/WEnvironment>\n
     wApp->environment().server(); }" WC_HAVE_ENVIRONMENT_SERVER)
 check_cxx_source_compiles("#include <Wt/Utils>\n int main() {
     Wt::Utils::md5(\"\"); }" WC_HAVE_WT_MD5)
-check_cxx_source_compiles("#include <Wt/WRasterImage>\n int main() {
-    &Wt::WRasterImage::getPixel; }"
+check_cxx_source_compiles("#include <Wt/WRasterImage>\n #include <Wt/WColor> \n
+    int main() { Wt::WRasterImage r(\"\",0,0); r.getPixel(0, 0); }"
     WC_HAVE_WRASTERIMAGE_GETPIXEL)
 check_cxx_source_compiles("#include <Wt/WFont>\n int main() {
     Wt::WFont().setSize(200); }" WC_HAVE_WFONT_SETSIZE_DOUBLE)
