e5147417bebd4fe3fe20018493ef8f195a71bd27
Boris Nagaev <bnagaev@gmail.com>
2013-09-14 23:52:35 +0400

    wnoref: create all widgets in app's constructor and then hide/show

--- examples/wnoref.cpp
+++ examples/wnoref.cpp
@@ -69,6 +69,27 @@ class WnorefApp : public WApplication {
 public:
     WnorefApp(const WEnvironment& env):
         WApplication(env) {
+        root()->clear();
+        root()->addWidget(new WText("Enter text to be shown only once"));
+        root()->addWidget(new WBreak);
+        root()->addWidget(new WText("Text is stored in the RAM, "
+                                    "not on hard drive"));
+        root()->addWidget(new WBreak);
+        textarea_ = new WTextArea(root());
+        textarea_->setRows(10);
+        textarea_->setColumns(80);
+        root()->addWidget(new WBreak);
+        WPushButton* get_link = new WPushButton("Get link!", root());
+        get_link->clicked().connect(this, &WnorefApp::do_get_link);
+        root()->addWidget(new WText("Store max "));
+        delay_ = new ConstrainedSpinBox(root());
+        delay_->setRange(1, 60);
+        delay_->setValue(5);
+        root()->addWidget(new WText(" minutes"));
+        root()->addWidget(new WBreak);
+        url_edit_ = new WLineEdit(root());
+        url_edit_->setTextSize(50);
+        url_edit_->hide();
         parser_ = new Parser(this);
         note_url_ = new StringNode(parser_);
         parser_->connect(parser_,
@@ -80,27 +101,8 @@ public:
     }
 
     void show_main() {
-        show_text("");
-    }
-
-    void show_text(const Wt::WString& text) {
-        root()->clear();
-        root()->addWidget(new WText("Enter text to be shown only once"));
-        root()->addWidget(new WBreak);
-        root()->addWidget(new WText("Text is stored in the RAM, "
-                                    "not on hard drive"));
-        root()->addWidget(new WBreak);
-        textarea_ = new WTextArea(text, root());
-        textarea_->setRows(10);
-        textarea_->setColumns(80);
-        root()->addWidget(new WBreak);
-        WPushButton* get_link = new WPushButton("Get link!", root());
-        get_link->clicked().connect(this, &WnorefApp::do_get_link);
-        root()->addWidget(new WText("Store max "));
-        delay_ = new ConstrainedSpinBox(root());
-        delay_->setRange(1, 60);
-        delay_->setValue(5);
-        root()->addWidget(new WText(" minutes"));
+        textarea_->setText("");
+        url_edit_->setText("");
     }
 
     void do_get_link() {
@@ -108,25 +110,23 @@ public:
         std::string key = rand_string(12);
         note->key_ = key;
         note->text_ = textarea_->text();
-        boost::mutex::scoped_lock lock(key_to_note_mutex_);
-        key_to_note_[key] = note;
+        {
+            boost::mutex::scoped_lock lock(key_to_note_mutex_);
+            key_to_note_[key] = note;
+        }
+        int delay_mins = delay_->corrected_value();
+        planning_.add(note, now() + delay_mins * td::MINUTE);
+        //
         note_url_->set_string(key);
         std::string url = environment().hostName() +
                           environment().deploymentPath() +
                           note_url_->full_path();
         boost::algorithm::replace_all(url, "//", "/");
         url = url_scheme() + "://" + url;
-        root()->clear();
-        WAnchor* main = new WAnchor(root());
-        main->setRefInternalPath(parser_->full_path());
-        main->setText("Main page");
-        root()->addWidget(new WBreak);
-        WLineEdit* url_edit = new WLineEdit(url, root());
-        url_edit->setTextSize(50);
-        doJavaScript(url_edit->jsRef() + ".select();");
-        doJavaScript(url_edit->jsRef() + ".focus();");
-        int delay_mins = delay_->corrected_value();
-        planning_.add(note, now() + delay_mins * td::MINUTE);
+        url_edit_->setText(url);
+        doJavaScript(url_edit_->jsRef() + ".select();");
+        doJavaScript(url_edit_->jsRef() + ".focus();");
+        url_edit_->show();
     }
 
     void show_note() {
@@ -134,12 +134,13 @@ public:
         std::string key = note_url_->value();
         Key2Note::iterator it = key_to_note_.find(key);
         if (it == key_to_note_.end()) {
-            show_text("No note with this key!");
+            textarea_->setText("No note with this key!");
         } else {
             NotePtr note = it->second;
-            show_text(note->text_);
+            textarea_->setText(note->text_);
             key_to_note_.erase(it); // show text only one time
         }
+        url_edit_->hide();
     }
 
 private:
@@ -147,6 +148,7 @@ private:
     StringNode* note_url_;
     WTextArea* textarea_;
     ConstrainedSpinBox* delay_;
+    WLineEdit* url_edit_;
 };
 
 WApplication* createWnorefApp(const WEnvironment& env) {
